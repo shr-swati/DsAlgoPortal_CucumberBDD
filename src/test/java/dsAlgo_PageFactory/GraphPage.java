e64d7e612659e1988fcd9da6274496be7641dc78
Angithagit <angithatom444@gmail.com>
2025-03-10 09:21:28 -0700

    initial commit[Angitha Tom]changes for tree and graph
    changes for tree and graph

---
+++ src/test/java/dsAlgo_PageFactory/GraphPage.java
@@ -0,0 +1,53 @@
+package dsAlgo_PageFactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class GraphPage {
+
+	WebDriver driver;
+ 	  
+	  @FindBy(xpath="//a[text()='Data Structures']")
+	  @CacheLookup 
+	  WebElement Data_structure;
+	  
+	  @FindBy(xpath="//*[@id=\"navbarCollapse\"]/div[1]/div/div/a[6]")
+	  @CacheLookup
+	  WebElement ds_graph;
+	  
+	  @FindBy(xpath="/html/body/div[2]")
+	  @CacheLookup
+	  WebElement login_msg;
+	  
+	  @FindBy(xpath="/html/body/div[2]/div[7]/div/div/a")
+	  @CacheLookup
+	  WebElement graph_getStarted_btn;
+	  
+	  public GraphPage(WebDriver driver) {
+	        this.driver = driver;
+	        PageFactory.initElements(driver, this);
+	        
+	    }
+	    
+	 
+	  public void ClickDS() {
+		  Data_structure.click();
+		}
+	  
+	  public void ClickDS_Graph() {
+		  ds_graph.click();
+		  
+	  }
+	  public String getmsg() {
+		   return login_msg.getText();  
+		  }
+	  public void ClickGetStarted() {
+		  			
+		  graph_getStarted_btn.click();
+
+		  }
+		  
+}
--- src/test/java/dsAlgo_utilities/BaseClass.java
+++ src/test/java/dsAlgo_utilities/BaseClass.java
@@ -1,11 +1,13 @@
 package dsAlgo_utilities;
 
+import dsAlgo_PageFactory.GraphPage;
 import dsAlgo_PageFactory.LoginPage;
+import dsAlgo_PageFactory.TreePage;
 
 
 public class BaseClass {
  public LoginPage loginPage;
- 
-
+ public TreePage treePage;
+ public GraphPage graphPage;
 }
 
